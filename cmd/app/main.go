f7ea51eec1c737fc05b8744b78d81952bffe0a32
Ravil Fattahov <fr@xpage.ru>
2024-11-15 01:05:14 +0500

    feat: ttl

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"combined-cache/internal/cache/lru"
 	"fmt"
+	"time"
 )
 
 func main() {
-	cache := lru.NewCache(3)
+	cache := lru.NewCache(3, time.Minute)
 	cache.Add("key1", 1)
-	value, exists := cache.Get("key1")
 
+	value, exists := cache.Get("key1")
+	fmt.Println(value, exists)
+
+	time.Sleep(time.Minute)
+
+	value, exists = cache.Get("key1")
 	fmt.Println(value, exists)
 }
--- internal/cache/lru/lru.go
+++ internal/cache/lru/lru.go
@@ -2,22 +2,26 @@ package lru
 
 import (
 	"container/list"
+	"time"
 )
 
 type CacheItem struct {
 	key   string
 	value interface{}
+	time  time.Time
 }
 
 type Cache struct {
 	capacity int
+	ttl      time.Duration
 	cache    map[string]*list.Element
 	list     *list.List
 }
 
-func NewCache(capacity int) *Cache {
+func NewCache(capacity int, ttl time.Duration) *Cache {
 	return &Cache{
 		capacity: capacity,
+		ttl:      ttl,
 		cache:    make(map[string]*list.Element),
 		list:     list.New(),
 	}
@@ -26,6 +30,7 @@ func NewCache(capacity int) *Cache {
 func (c *Cache) Add(key string, value interface{}) {
 	if element, exists := c.cache[key]; exists {
 		element.Value.(*CacheItem).value = value
+		element.Value.(*CacheItem).time = time.Now()
 		c.list.MoveToFront(element)
 		return
 	}
@@ -34,24 +39,35 @@ func (c *Cache) Add(key string, value interface{}) {
 		c.removeLast()
 	}
 
-	newItem := &CacheItem{key: key, value: value}
+	newItem := &CacheItem{
+		key:   key,
+		value: value,
+		time:  time.Now(),
+	}
 	listElement := c.list.PushFront(newItem)
 	c.cache[key] = listElement
 }
 
 func (c *Cache) Get(key string) (interface{}, bool) {
 	if element, exists := c.cache[key]; exists {
-		c.list.MoveToFront(element)
-		return element.Value.(*CacheItem).value, true
+		if time.Since(element.Value.(*CacheItem).time) < c.ttl {
+			c.list.MoveToFront(element)
+			return element.Value.(*CacheItem).value, true
+		}
+		c.removeElement(element)
 	}
 
 	return nil, false
 }
 
 func (c *Cache) removeLast() {
-	item := c.list.Back()
-	if item != nil {
-		c.list.Remove(item)
-		delete(c.cache, item.Value.(*CacheItem).key)
+	element := c.list.Back()
+	if element != nil {
+		c.removeElement(element)
 	}
+}
+
+func (c *Cache) removeElement(element *list.Element) {
+	c.list.Remove(element)
+	delete(c.cache, element.Value.(*CacheItem).key)
 }
